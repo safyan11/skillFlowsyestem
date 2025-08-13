3654862a063f5e34f5af88c4199104014e3da4ee
riss group <wahadsafyan410@gmail.com>
2025-08-14 01:12:56 +0500

    first commits

--- build/digital.php
+++ build/digital.php
@@ -8,11 +8,7 @@
 
 
 <!-- parralex  -->
-   <section class="h-96 bg-fixed bg-center bg-no-repeat bg-cover" style="background-image: url('./assets/img/services.jpg')">
-  <div class="h-96 flex items-center justify-center bg-black bg-opacity-60">
-    <h1 class="md:text-4xl text-2xl font-bold text-white">Digital Markiting</h1>
-  </div>
-</section>
+ 
 
 <!-- included section  -->
 
